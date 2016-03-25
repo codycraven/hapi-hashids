5b485303729ecb46ad2265f92a2facd9b042d004
Cody Craven <cody@cravencode.com>
2016-03-25 04:31:09 +0000

    Reached 100% code coverage.

--- test/index.js
+++ test/index.js
@@ -23,6 +23,118 @@ const expect = Code.expect;
 
 describe('Hashids', () => {
 
+    it('assigns salt correctly', (done) => {
+
+        const server = new Hapi.Server();
+        const mySalt = 'test salt';
+        server.connection();
+
+        server.register({
+            register: Hashids,
+            options: {
+                salt: mySalt
+            }
+        }, (err) => {
+
+            if (err) {
+                throw err;
+            }
+
+            expect(server.plugins['hapi-hashids'].salt).to.equal(mySalt);
+            done();
+        });
+    });
+
+    it('defaults to correct minHashLength', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register({
+            register: Hashids,
+            options: {
+                salt: 'this is my salt'
+            }
+        }, (err) => {
+
+            if (err) {
+                throw err;
+            }
+
+            expect(server.plugins['hapi-hashids'].minHashLength).to.equal(0);
+            done();
+        });
+    });
+
+    it('assigns minHashLength correctly', (done) => {
+
+        const server = new Hapi.Server();
+        const myHashLength = 8;
+        server.connection();
+
+        server.register({
+            register: Hashids,
+            options: {
+                salt: 'this is my salt',
+                minHashLength: myHashLength
+            }
+        }, (err) => {
+
+            if (err) {
+                throw err;
+            }
+
+            expect(server.plugins['hapi-hashids'].minHashLength).to.equal(myHashLength);
+            const encode = server.plugins['hapi-hashids'].encode(1);
+            expect(encode).to.have.length(myHashLength);
+            done();
+        });
+    });
+
+    it('defaults to correct alphabet', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+
+        server.register({
+            register: Hashids,
+            options: {
+                salt: 'this is my salt'
+            }
+        }, (err) => {
+
+            if (err) {
+                throw err;
+            }
+
+            expect(server.plugins['hapi-hashids'].alphabet).to.match(/^[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890]+$/);
+            done();
+        });
+    });
+
+    it('assigns alphabet correctly', (done) => {
+
+        const server = new Hapi.Server();
+        const myAlphabet = '0123456789abcdef';
+        server.connection();
+
+        server.register({
+            register: Hashids,
+            options: {
+                salt: 'this is my salt',
+                alphabet: myAlphabet
+            }
+        }, (err) => {
+
+            if (err) {
+                throw err;
+            }
+
+            expect(server.plugins['hapi-hashids'].alphabet).to.match(new RegExp('^[' + myAlphabet + ']+$'));
+            done();
+        });
+    });
+
     it('encodes and decodes an integer', (done) => {
 
         const server = new Hapi.Server();
@@ -58,7 +170,6 @@ describe('Hashids', () => {
                 salt: 'this is my salt'
             }
         }, (err) => {
-
 
             if (err) {
                 throw err;
